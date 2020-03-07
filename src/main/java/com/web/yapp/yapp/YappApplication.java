1a064fdc894344a08ee83d3795295f243ee0682b
황선아 <tjsdk2769@gmail.com>
2020-03-07 14:33:50 +0900

    Add config setting

---
+++ src/main/java/com/web/yapp/yapp/YappApplication.java
@@ -0,0 +1,13 @@
+package com.web.yapp.yapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YappApplication.class, args);
+    }
+
+}
