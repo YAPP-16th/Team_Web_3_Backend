02649a99ed5064ed251abb64ce84df709687b3a1
황선아 <tjsdk2769@gmail.com>
2020-04-09 14:29:43 +0900

    Add User Entity Structure & Fix OAuthAttributes

--- src/main/java/com/web/yapp/server/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/web/yapp/server/config/auth/CustomOAuth2UserService.java
@@ -59,7 +59,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
     private User saveOrUpdate(OAuthAttributes attributes){
         User user = userRepository.findByEmail(attributes.getEmail())
                 .map(entity -> entity.update(attributes.getName(),
-                        attributes.getPicture()))
+                        attributes.getProfile_url()))
                 .orElse(attributes.toEntity());
         return userRepository.save(user);
     }
--- src/main/java/com/web/yapp/server/domain/user/User.java
+++ src/main/java/com/web/yapp/server/domain/user/User.java
@@ -12,6 +12,7 @@ import javax.persistence.*;
 @Entity
 public class User {
     @Id
+    @Column(name = "user_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -22,23 +23,35 @@ public class User {
     private String email;
 
     @Column
-    private String picture;
+    private String profile_url;
+
+    @Column //연령대
+    private int age;
+
+    @Column //별명
+    private String nickname;
+
+    @Column //성별
+    private String gender;
+
+    @Column //생일
+    private String birthday;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
 
     @Builder
-    public User(String name,String email, String picture, Role role){
+    public User(String name,String email, String profile_url, Role role){
         this.name = name;
         this.email = email;
-        this.picture = picture;
+        this.profile_url = profile_url;
         this.role = role;
     }
 
-    public User update(String name, String picture){
+    public User update(String name, String profile_url){
         this.name = name;
-        this.picture = picture;
+        this.profile_url = profile_url;
         return this;
     }
 
