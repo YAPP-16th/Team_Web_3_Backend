7d022c20b8f8a9ba1a995fcaeb86dc1bf62e5590
황선아 <tjsdk2769@gmail.com>
2020-04-10 09:53:32 +0900

    Add Artist Entity Structure

--- src/main/java/com/web/yapp/server/domain/artist/Artist.java
+++ src/main/java/com/web/yapp/server/domain/artist/Artist.java
@@ -1,12 +1,36 @@
-//package com.web.yapp.server.domain.artist;
-//
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//
-//import javax.persistence.Entity;
-//
-//@Getter
-//@NoArgsConstructor
-//@Entity
-//public class Artist {
-//}
+package com.web.yapp.server.domain.artist;
+
+import com.web.yapp.server.domain.user.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "TUNA_ARTIST")
+@Entity
+public class Artist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "artist_id")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column //경력
+    private String career;
+
+    @Column(nullable = false) //아티스트 이름
+    private String name;
+
+    @Column(nullable = false) //프로필 문구
+    private String introduction;
+
+    @Column //프로필 사진 url
+    private String profile_url;
+
+
+}
