9111108437e0f87602892570b7953b1c925bc6ba
황선아 <tjsdk2769@gmail.com>
2020-04-10 10:01:40 +0900

    Add Bookmark Entity Structure & BookmarkRepository

---
+++ src/main/java/com/web/yapp/server/domain/bookmark/Bookmark.java
@@ -0,0 +1,28 @@
+package com.web.yapp.server.domain.bookmark;
+
+import com.web.yapp.server.domain.artist.Artist;
+import com.web.yapp.server.domain.user.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "TUNA_BOOKMARK")
+@Entity
+public class Bookmark {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bookmark_id")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "artist_id")
+    private Artist artist;
+
+    @ManyToOne // 북마크(n) -> 유저(1)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
