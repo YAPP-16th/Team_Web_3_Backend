a2955a4a0b28ad5e23c78c1629fd38eb67824664
황선아 <tjsdk2769@gmail.com>
2020-04-14 21:42:57 +0900

    Fix uploader view, upload api method

--- src/main/java/com/web/yapp/server/controller/FileController.java
+++ src/main/java/com/web/yapp/server/controller/FileController.java
@@ -16,14 +16,16 @@ import java.io.IOException;
 public class FileController {
     private final S3Uploader s3Uploader;
 
-    @GetMapping("/view/up")
+    @GetMapping("/file") // 파일 업로드 테스트용 뷰
     public String uploadView(){
         return "upload";
     }
 
-    @PostMapping("/upload")
+    @PostMapping("/upload") // 업로드 API. 파일 URL 리턴
     @ResponseBody
     public String upload(@RequestParam("data") MultipartFile multipartFile) throws IOException {
-        return s3Uploader.upload(multipartFile, "static");
+        String result = s3Uploader.upload(multipartFile, "static");
+        System.out.println("결과 URL "+result); //결과 URL console에 출력
+        return result;
     }
 }
