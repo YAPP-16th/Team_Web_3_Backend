ab41f8caa1730b7ba0c42e2291ff4220e70d22ab
황선아 <tjsdk2769@gmail.com>
2020-04-22 21:33:22 +0900

    Add SongRepository

---
+++ src/main/java/com/web/yapp/server/controller/SongController.java
@@ -0,0 +1,8 @@
+package com.web.yapp.server.controller;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+//@RestController
+public class SongController {
+
+}
--- src/main/java/com/web/yapp/server/domain/repository/SongRepository.java
+++ src/main/java/com/web/yapp/server/domain/repository/SongRepository.java
@@ -3,5 +3,11 @@ package com.web.yapp.server.domain.repository;
 import com.web.yapp.server.domain.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SongRepository extends JpaRepository<Song, Long> {
+    List<Song> findAll();
+    Optional<Song> findById(Long id);
+    List<Song> findByMusicianId(Long id);
 }
