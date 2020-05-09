8b4927f9e5e99a157b6076aad929fb5e3955c45b
황선아 <tjsdk2769@gmail.com>
2020-05-09 11:23:13 +0900

    Fix SongController

--- src/main/java/com/web/yapp/server/controller/SongController.java
+++ src/main/java/com/web/yapp/server/controller/SongController.java
@@ -1,25 +1,16 @@
 package com.web.yapp.server.controller;
 
 import com.web.yapp.server.controller.dto.SongDto;
-import com.web.yapp.server.domain.Song;
-import com.web.yapp.server.domain.repository.SongRepository;
-import com.web.yapp.server.domain.service.S3Uploader;
 import com.web.yapp.server.domain.service.SongService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/song")
 public class SongController {
     private final SongService songService;
-    private final SongRepository songRepository;
-
 
     /**
      *
@@ -30,34 +21,11 @@ public class SongController {
     public List<SongDto> songsByMusician(@PathVariable Long musicianId){
         return songService.findSongByMusicianId(musicianId);
     }
-    /*
-    * 업로드하면 url반환도 해주고 song save도 될 수있도록 구현하기
-    * */
-//    @PostMapping //노래추가
-//    public SongDto createSong(@RequestParam String title, @RequestParam String ){ //save
-//        SongDto songDto = new SongDto();
-//        songRepository.save(songDto.toEntity());
-//        return songDto;
-//    }
 
-    // song 등록 api를 굳이 만들 필요 없음 service 통해서 파일만 업로드 해주면됨
-//    @PostMapping("/represent") // 대표곡 업로드 API. 파일 URL 리턴
-//    @ResponseBody
-//    public SongDto upload(@RequestParam("data")  List<MultipartFile> multipartFiles, String title, Long musicainId) throws IOException {
-//        String coverUrl = s3Uploader.upload(multipartFiles.get(0), "static");
-//        String songUrl = s3Uploader.upload(multipartFiles.get(1),"static");
-//        SongDto songDto = new SongDto();
-//        songDto.builder()
-//                .title(title)
-//                .build();
-//
-//        return result;
-//    }
 //    @PutMapping
 //    public SongDto updateSong(){} //save
 //
 //    @DeleteMapping
 //    public SongDto deleteSong(){} //delete
-
-
+    
 }
