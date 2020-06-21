890576b57049a4b89a8abaafec3f815593ac9353
황선아 <tjsdk2769@gmail.com>
2020-06-21 15:03:17 +0900

    Add songId in SongMainResponseDto

--- src/main/java/com/web/yapp/server/controller/dto/SongMainResponseDto.java
+++ src/main/java/com/web/yapp/server/controller/dto/SongMainResponseDto.java
@@ -7,11 +7,13 @@ import lombok.Setter;
 @Setter
 @Getter
 public class SongMainResponseDto {
+    private Long songId;
     private String title;
     private String coverUrl;
     private String songUrl;
 
     public SongMainResponseDto(Song Entity){
+        this.songId = Entity.getId();
         this.title = Entity.getTitle();
         this.coverUrl = Entity.getCoverUrl();
         this.songUrl = Entity.getSongUrl();
